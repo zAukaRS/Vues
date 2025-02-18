e12a4c74cfdcff0af0242cab0fe216f8f46a114d
zAukaRS <evgenii-savostin10@mail.ru>
2025-02-18 14:34:53 +0700

    the review block

--- js/main.js
+++ js/main.js
@@ -1,3 +1,65 @@
+Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+      <p v-if="errors.length">
+        <b>Пожалуйста, исправьте следующие ошибки:</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+      <p>
+        <label for="name">Имя:</label>
+        <input id="name" v-model="name" placeholder="Имя">
+      </p>
+      <p>
+        <label for="review">Отзыв:</label>
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+      <p>
+        <label for="rating">Оценка:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+      <p>
+        <input type="submit" value="Отправить">
+      </p>
+    </form>
+  `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        };
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                };
+                this.$emit('review-submitted', productReview);
+                this.name = null;
+                this.review = null;
+                this.rating = null;
+            } else {
+                if (!this.name) this.errors.push("Имя обязательно.");
+                if (!this.review) this.errors.push("Отзыв обязателен.");
+                if (!this.rating) this.errors.push("Оценка обязательна.");
+            }
+        }
+    }
+});
+
+
 Vue.component('product', {
     props: {
         premium: {
