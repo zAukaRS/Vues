d96b6dffe246de1a9b102358f08517fbadacca6a
zAukaRS <evgenii-savostin10@mail.ru>
2025-02-18 14:24:19 +0700

    Rendering

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+    }
+});
